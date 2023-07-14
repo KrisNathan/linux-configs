f207871eb46b9b0c468c0f646882df07c0bc9645
KrisNathan <kristophernathanael2005@gmail.com>
2023-07-14 16:27:08 +0700

    added comment about helix

--- symlink.sh
+++ symlink.sh
@@ -7,6 +7,7 @@ ln -s $DIR/dotconfig/alacritty ~/.config/alacritty
 ln -s $DIR/dotconfig/i3 ~/.config/i3
 ln -s $DIR/dotconfig/i3status ~/.config/i3status
 ln -s $DIR/dotconfig/sway ~/.config/sway
+# idk about helix
 
 ln -s $DIR/home/.zshrc ~/.zshrc
 ln -s $DIR/home/.profile ~/.profile
