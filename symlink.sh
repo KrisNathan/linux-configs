9d105df0e31f0dc8b13658511788ac3d06d08f7b
KrisNathan <kristophernathanael2005@gmail.com>
2023-07-16 02:09:01 +0700

    fix: helix configs not working when runtime config exists

--- symlink.sh
+++ symlink.sh
@@ -9,7 +9,8 @@ ln -s $DIR/dotconfig/i3status ~/.config/i3status
 ln -s $DIR/dotconfig/sway ~/.config/sway
 
 # idk about helix
-ln -s $DIR/dotconfig/helix ~/.config/helix
+ln -s $DIR/dotconfig/helix/config.toml ~/.config/helix/config.toml
+ln -s $DIR/dotconfig/helix/language.toml ~/.config/helix/language.toml
 
 ln -s $DIR/home/.zshrc ~/.zshrc
 ln -s $DIR/home/.profile ~/.profile
